6e6934284635c3229e0febcd321eb9d11e70fc91
ctroubit <ctroubit@uwo.ca>
2023-11-02 15:44:04 -0400

    Setting up base server script

--- server/server-script.js
+++ server/server-script.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const fs = require('fs')
 const app = express();
+let s_info_data;
+fs.readFile('superheroes/superhero_info.json','utf8',(err,data)=>{
+    if(err){
+        console.error('Error:',err)
+        return;
+    }
+    s_info_data = JSON.parse(data);
+})
 
 app.get('/',(req,res)=>{
     res.send('Hello World')
 });
 
-app.listen(3000,)
+app.get('/api/superhero_info',(req,res)=>{
+    res.send(s_info_data)
+})
+
+app.get('api/superhero_info/:id',(req,res)=>{
+    const superhero = s_info_data.find(s => s.name)
+})
+const port = process.env.ACSvcPort || 3000
+app.listen(port,()=>console.log(`Listening on port ${port}...`))
