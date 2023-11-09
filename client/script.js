0d779fc2a67201078fe256c0c3384ebfd30241ca
ctroubit <ctroubit@uwo.ca>
2023-11-08 19:00:25 -0500

    Added ability to search for multiple attributes

--- client/script.js
+++ client/script.js
@@ -1,15 +1,20 @@
 function fetchSuperheroInfo() {
-    const text = document.getElementById('superheroId').value;
-    const output = document.getElementById('listBox');
-
+    let output = document.getElementById('listBox');
+    let name = document.getElementById('superheroId').value;
+    let power = document.getElementById('powersSelection').value;
+    let race = document.getElementById('raceSelection').value;
+    let publisher = document.getElementById('publisherSelection').value;
+    console.log(name,power,race,publisher)
     output.innerHTML = '';
 
-    if (text === '') {
-        output.textContent = '';
-        return;
+
+    let url = `http://localhost:17532/api/superheroes?${name ? 'name=' + name : ''}${power ? '&power=' + power : ''}${race ? '&Race=' + race : ''}${publisher ? '&Publisher=' + publisher : ''}`;
+
+    if (url.endsWith('&')) {
+        url = url.slice(0, -1);
     }
 
-    fetch(`http://localhost:17532/api/superheroes/name/${text}`).then(response => {
+    fetch(url).then(response => {
         if (!response.ok) {
             let paragraph = document.getElementById('startP');
             paragraph.style.display = 'block';
@@ -18,72 +23,70 @@ function fetchSuperheroInfo() {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         return response.json();
-    }).then(superheroes => {
+    }).then(superhero => {
+        console.log(superhero)
         let paragraph = document.getElementById('startP');
         paragraph.style.display = 'none';
 
-        for (const superhero of superheroes) {
-            let div = document.createElement('div');
-            div.style.background = '#3500D3';
-            div.style.width = '500px';
-            div.style.alignContent = 'center';
-            div.style.marginBottom = '10px';
-            div.style.display = 'flex';
-            div.style.justifyContent = 'center';
-            div.setAttribute('class', 'superheroDiv');
-            div.style.color = 'WHITE';
+        let div = document.createElement('div');
+        div.style.background = '#3500D3';
+        div.style.width = '750px';
+        div.style.alignItems = 'center';
+        div.style.alignContent = 'center';
+        div.style.marginBottom = '10px';
+        div.style.display = 'flex';
+        div.style.justifyContent = 'center';
+        div.setAttribute('class', 'superheroDiv');
+        div.style.color = 'WHITE';
 
-            let ul = document.createElement('ul');
-            ul.style.listStyleType = 'none';
-            ul.style.padding = '0';
-            ul.style.display = 'flex';
-            ul.style.flexWrap = 'wrap';
+        let ul = document.createElement('ul');
+        ul.style.listStyleType = 'none';
+        ul.style.padding = '0';
+        ul.style.display = 'flex';
+        ul.style.flexWrap = 'wrap';
 
-            for (const [key, value] of Object.entries(superhero)) {
-                if (key !== '_id') { // Exclude the "_id" field
-                    let li = document.createElement('li');
-                    li.style.listStyleType = 'none';
-                    li.style.width = '100%';
-                    li.style.textAlign = 'center';
-                    li.style.padding = '10px';
-                    li.style.flex = '0 0 calc(33.33% - 20px)';
-                    li.textContent = `${key}: ${JSON.stringify(value)}`;
-                    ul.appendChild(li);
-                }
+        for (const [key, value] of Object.entries(superhero)) {
+            if (key !== '_id') {
+                let li = document.createElement('li');
+                li.style.listStyleType = 'none';
+                li.style.width = '100%';
+                li.style.textAlign = 'center';
+                li.style.flex = '0 0 calc(100% - 20px)';
+                li.style.padding = '10px';
+
+
+                let contentDiv = document.createElement('div');
+                contentDiv.style.maxWidth = '1000px';
+
+
+                contentDiv.textContent = `${key}: ${JSON.stringify(value)}`;
+
+
+                let addButton = document.createElement('button');
+                addButton.textContent = 'Add to List';
+                addButton.addEventListener('click', function () {
+                    addToMyList(superhero);
+                });
+
+                li.appendChild(contentDiv);
+                li.appendChild(addButton);
+                ul.appendChild(li);
             }
-
-            div.appendChild(ul);
-
-            output.appendChild(div);
         }
+
+        div.appendChild(ul);
+
+        output.appendChild(div);
     }).catch(error => console.error('Error: ', error));
+
 }
 
-function fetchSuperheroPower(){
-    const text = document.getElementById('powersId').value
-    const output = document.getElementById('output')
-
-    if(text ===``){
-        output.textContent = ''
-        return;
-    }
-
-    fetch(`http://localhost:17532/api/superheroes/${text}/powers`).then(response =>{
-        if(!response.ok)
-            throw new Error(`HTTP error! status: ${response.status}`)
-
-        return response.json()
-    })
-        .then(powers =>{
-            output.textContent = JSON.stringify(powers)
-            console.log(JSON.stringify(powers))
-        }).catch(error =>console.error('Error',error))
-}
 
 async function createListBox() {
     let whiteBox = document.getElementById('listContainer');
     let paragraph = document.getElementById('startP');
     let textbox = document.createElement('input');
+    let listBoxCreated = false;
     textbox.style.alignContent = 'center';
     textbox.style.display = 'flex';
     paragraph.textContent = 'Enter a list name!'
@@ -91,76 +94,83 @@ async function createListBox() {
     let container = document.getElementById('startContainer');
     container.appendChild(textbox)
 
+        textbox.addEventListener('keydown', async function (event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
 
-    textbox.addEventListener('keydown', async function (event) {
-        if (event.keyCode === 13) {
-            event.preventDefault();
+                let userInput = textbox.value;
 
-            let userInput = textbox.value;
+                const data = {
+                    listName: `${userInput}`,
+                    superheroes: []
+                }
 
-            const data = {
-                listName: `${userInput}`,
-                superheroes: []
-            }
+                await fetch(`http://localhost:17532/api/lists/`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                });
 
-            await fetch(`http://localhost:17532/api/lists/`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            });
-
-            let button = document.createElement('button');
-            textbox.style.display = 'none'
-            button.style.background = '#8EE4AF';
-            button.style.alignContent = 'center';
-            button.style.width = (whiteBox.offsetWidth - 2) + 'px';
-            button.style.height = '25px';
-            button.style.top = '1px'
-            button.style.marginBottom = '1px';
-            button.style.display = 'flex';
-            button.style.border = '1px solid black';
-            button.style.justifyContent = 'center';
-            button.style.marginLeft = '1px';
-            button.setAttribute('id', 'listButton');
-            button.style.color = '#4B4B4B';
-            button.textContent = userInput;
-
-            button.addEventListener('mouseenter', function () {
-                button.style.backgroundColor = '#4CAF50';
-                button.style.color = 'white';
-            });
-
-            button.addEventListener('mouseleave', function () {
-                button.style.backgroundColor = '#8EE4AF';
+                let button = document.createElement('button');
+                textbox.style.display = 'none'
+                button.style.background = '#8EE4AF';
+                button.style.alignContent = 'center';
+                button.style.width = (whiteBox.offsetWidth - 2) + 'px';
+                button.style.height = '25px';
+                button.style.top = '1px'
+                button.style.marginBottom = '1px';
+                button.style.display = 'flex';
+                button.style.border = '1px solid black';
+                button.style.justifyContent = 'center';
+                button.style.marginLeft = '1px';
+                button.setAttribute('id', 'listButton');
                 button.style.color = '#4B4B4B';
-            });
+                button.textContent = userInput;
 
-            whiteBox.appendChild(button);
+                button.addEventListener('mouseenter', function () {
+                    button.style.backgroundColor = '#4CAF50';
+                    button.style.color = 'white';
+                });
 
-            let othContainer = document.getElementById('colouredBlock');
-            let header = document.getElementById('labHeader')
+                button.addEventListener('mouseleave', function () {
+                    button.style.backgroundColor = '#8EE4AF';
+                    button.style.color = '#4B4B4B';
+                });
 
-            header.style.marginTop = '-305px'
-            let listBox = document.createElement('div');
-            listBox.setAttribute('id', 'listBox');
-            listBox.style.marginTop = (-635) + 'px';
-            listBox.style.marginLeft = (othContainer.offsetLeft + 175) + 'px';
-            listBox.style.width = '86.25vw';
-            listBox.style.height = ' 92.75vh';
-            listBox.style.position = 'relative';
-            listBox.style.overflow = 'auto';
-            listBox.style.backgroundColor = 'white';
-            othContainer.appendChild(listBox);
+                whiteBox.appendChild(button);
 
-            paragraph.style.display = 'none';
-        }
-    });
-}
+                if (!listBoxCreated) {
+                    listBoxCreated = true;
+
+                    // Create listBox
+                    let othContainer = document.getElementById('colouredBlock');
+                    let header = document.getElementById('labHeader');
+                    let listBox = document.createElement('div');
+                    listBox.setAttribute('id', 'listBox');
+                    listBox.style.marginTop = (-637) + 'px';
+                    listBox.style.marginLeft = (othContainer.offsetLeft + 175) + 'px';
+                    listBox.style.width = '86.25vw';
+                    listBox.style.height = ' 92.75vh';
+                    listBox.style.overflow = 'auto';
+                    listBox.style.alignItems = 'center'
+                    listBox.style.justifyContent = 'center'
+                    listBox.style.alignContent = 'center';
+                    listBox.style.display = 'flex';
+                    listBox.style.flexDirection = 'column'
+                    listBox.style.backgroundColor = 'white';
+                    othContainer.appendChild(listBox);
+                }
+
+                paragraph.style.display = 'none';
+            }
+        });
+    }
+
 
 function getPublisher(){
-    fetch('http://localhost:17532/api/superheroes')
+    fetch('http://localhost:17532/api/superheroes/single')
         .then(response => {
             if(!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
@@ -184,7 +194,6 @@ function getPublisher(){
             for (let i of publishers) {
                 let option = document.createElement("option");
                 option.text = i;
-                console.log(i)
                 select.add(option);
             }
         })
@@ -193,7 +202,7 @@ function getPublisher(){
 getPublisher()
 
 function getRace(){
-    fetch('http://localhost:17532/api/superheroes')
+    fetch('http://localhost:17532/api/superheroes/single')
         .then(response => {
             if(!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
@@ -268,6 +277,47 @@ function fetchLists(){
         }
     )
 }
+
+function getPowers() {
+
+    fetch(`http://localhost:17532/api/superheroes/single`).then(response => {
+        if (!response.ok)
+            throw new Error(`HTTP error! status: ${response.status}`);
+
+        return response.json();
+    })
+        .then(data => {
+
+            const powers = data.powers;
+            const fEntry = powers[0];
+            const allPowers = new Set();
+
+            const keys = Object.keys(fEntry);
+            keys.forEach(key => {
+                if (key !== 'hero_names' && key !== '_id') {
+                    allPowers.add(key);
+                }
+            });
+
+            let select = document.getElementById('powersSelection');
+            for (let i of allPowers) {
+
+                let option = document.createElement("option");
+                option.text = i;
+                select.add(option);
+            }
+        })
+        .catch(error => console.error('Error', error));
+}
+
+function clearSearch(){
+    document.getElementById('superheroId').value = '';
+    document.getElementById('powersSelection').selectedIndex = 0;
+    document.getElementById('raceSelection').selectedIndex = 0;
+    document.getElementById('publisherSelection').selectedIndex = 0;
+}
+
+getPowers()
 
 fetchLists()
 
--- server/server-script.js
+++ server/server-script.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fs= require('fs')
 const cors = require('cors')
 const path = require('path');
 const {connectToDb, getDb} = require('./db')
@@ -18,7 +17,83 @@ app.use(cors())
 app.use(express.static(path.join(__dirname, 'client')));
 app.use(express.json())
 
+// app.get('/api/superheroes', async (req, res) => {
+//     try {
+//         const infoCursor = db.collection('info').find();
+//         const powersCursor = db.collection('powers').find();
+//
+//         const s_info_data = await infoCursor.toArray();
+//         const s_powers_data = await powersCursor.toArray();
+//
+//         const combinedData = {
+//             superheroes: s_info_data,
+//             powers: s_powers_data
+//         };
+//
+//         res.status(200).json(combinedData);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: 'Could not fetch data' });
+//     }
+// });
+
 app.get('/api/superheroes', async (req, res) => {
+    try {
+        let query = {};
+
+        if (req.query.name) {
+            query.name = { $regex: new RegExp(`^${req.query.name}`, 'i') };
+        }
+
+        if (req.query.Race) {
+            query.Race = req.query.Race;
+        }
+
+        if (req.query.Publisher) {
+            query.Publisher = req.query.Publisher;
+        }
+
+        console.log('Initial query:', query);
+
+        const matchingSuperheroes = await db.collection('info').find(query).toArray();
+
+        if (req.query.power) {
+            if (matchingSuperheroes.length === 0) {
+                console.log('No matching superheroes found');
+                res.status(404).send('No matching superheroes found');
+                return;
+            }
+
+            const heroNames = matchingSuperheroes.map(hero => hero.name);
+
+            const powers = await db.collection('powers').find({ hero_names: { $in: heroNames } }).toArray();
+
+            const superheroesWithPowers = matchingSuperheroes.filter(hero => {
+                const heroPowers = powers.find(power => power.hero_names.includes(hero.name));
+                const powerBeingLookedFor = req.query.power;
+
+                return heroPowers && heroPowers[powerBeingLookedFor] === 'True';
+            });
+
+            if (superheroesWithPowers.length === 0) {
+                console.log(`No matching superheroes found for power: ${req.query.power}`);
+                res.status(404).send(`No matching superheroes found for power: ${req.query.power}`);
+                return;
+            }
+
+            console.log('Sending matching superheroes with powers');
+            res.status(200).json(superheroesWithPowers);
+        } else {
+            console.log('Sending matching superheroes');
+            res.status(200).json(matchingSuperheroes);
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Could not fetch superhero data', message: error.message });
+    }
+});
+
+app.get('/api/superheroes/single',async(req,res)=>{
     try {
         const infoCursor = db.collection('info').find();
         const powersCursor = db.collection('powers').find();
@@ -36,38 +111,42 @@ app.get('/api/superheroes', async (req, res) => {
         console.error(error);
         res.status(500).json({ error: 'Could not fetch data' });
     }
-});
+})
 
-app.get('/api/superheroes/id/:id', async (req, res) => {
+app.get('/api/superheroes/single/:searchBy/:value', async (req, res) => {
     try {
-        const superheroId = parseInt(req.params.id);
-        const superhero = await db.collection('info').findOne({ id: superheroId });
+        const searchBy = req.params.searchBy;
+        const value = req.params.value;
+        let query = {};
 
-        if (!superhero) {
-            res.status(404).send('Superhero not found');
-            return;
+        switch (searchBy) {
+            case 'id':
+                query = { id: parseInt(value) };
+                break;
+            case 'name':
+                query = { name: { $regex: new RegExp(`^${value}`, 'i') } };
+                break;
+            case 'power':
+                query = { powers: { $elemMatch: { $eq: value, $eq: "True" } } };
+                break;
+            case 'publisher':
+                query = { Publisher: value };
+                break;
+            case 'race':
+                query = { Race: value };
+                break;
+            default:
+                res.status(400).json({ error: 'Invalid search criteria' });
+                return;
         }
 
-        res.status(200).json(superhero);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Could not fetch superhero data' });
-    }
-});
-
-app.get('/api/superheroes/name/:name', async (req, res) => {
-    try {
-        const searchName = req.params.name;
-        const regex = new RegExp(`^${searchName}`, 'i');
-
-        const infoCursor = db.collection('info').find({ name: regex });
-        const matchingSuperheroes = await infoCursor.toArray();
+        const matchingSuperheroes = await db.collection('info').find(query).toArray();
 
         if (matchingSuperheroes.length === 0) {
-            console.log('Superheroes not found');
-            res.status(404).send('Superheroes not found');
+            console.log(`No superheroes found for ${searchBy}: ${value}`);
+            res.status(404).send(`No superheroes found for ${searchBy}: ${value}`);
         } else {
-            console.log('Sending matching superheroes');
+            console.log(`Sending matching superheroes for ${searchBy}: ${value}`);
             res.status(200).json(matchingSuperheroes);
         }
     } catch (error) {
@@ -76,7 +155,7 @@ app.get('/api/superheroes/name/:name', async (req, res) => {
     }
 });
 
-app.get('/api/superheroes/hero_names/:hero_names', async (req, res) => {
+app.get('/api/superheroes/:hero_names', async (req, res) => {
     try {
         const searchName = req.params.hero_names;
         const regex = new RegExp(`^${searchName}`, 'i');
@@ -122,17 +201,66 @@ app.put('/api/lists/:listName',(req,res)=>{
     const list = lists.find(l => l.listName === req.params.listName)
 
 })
-app.post('/api/lists',(req,res)=>{
-const {listName, superheroes} = req.body
+app.post('/api/lists', (req, res) => {
+    const { listName, superheroes } = req.body;
+    db.collection('lists').insertOne({ listName, superheroes })
+        .then(result => {
+            res.status(201).json(result);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
 
-    const list = {
-    listName,superheroes
+
+app.get('/api/lists', async (req, res) => {
+    try {
+        const lists = await db.collection('lists').find({}).toArray();
+        res.json(lists);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Could not fetch lists' });
     }
+});
 
-    lists.push(list)
-    res.send(list)
+app.get('/api/lists/:listName', async (req,res) =>{
+    try{
+        const list = await db.collection('lists').find({listName: req.params.listName}).toArray()
+        res.json(list)
+    }catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Could not fetch lists' });
+    }
 })
+app.get('/api/superheroes', async (req, res) => {
+    try {
+        const { name, power, race, publisher } = req.query;
 
-app.get('/api/lists',(req,res)=>{
-    res.json(lists)
-})
+        // Build the query object based on the provided parameters
+        const query = {};
+
+        if (name) {
+            query.name = new RegExp(`^${name}`, 'i');
+        }
+
+        if (power) {
+            query.powers = { $elemMatch: { powerName: power } };
+        }
+
+        if (race) {
+            query.race = race;
+        }
+
+        if (publisher) {
+            query.publisher = publisher;
+        }
+
+        const superheroes = await db.collection('info').find(query).toArray();
+
+        res.status(200).json(superheroes);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Could not fetch data' });
+    }
+});
+
